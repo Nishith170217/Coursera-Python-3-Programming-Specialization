42824db9c09fcf6d29ae1d7589f45632e11f770e
Nishith <45823099+Nishith170217@users.noreply.github.com>
2020-05-26 01:20:34 +0800

    Update and rename Write code that uses iteration to print out the length of each element of the list stored in str_list to str_list.py

--- str_list.py
+++ str_list.py
@@ -1,3 +1,4 @@
+#Write code that uses iteration to print out the length of each element of the list stored in str_list
 str_list = ["hello", "", "goodbye", "wonderful", "I love Python"]
 
 # Write your code here.
