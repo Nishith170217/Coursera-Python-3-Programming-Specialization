1d0721a9f1aaa628fe70bd1ed0cd66a4b981b788
Nishith <45823099+Nishith170217@users.noreply.github.com>
2020-05-26 01:19:23 +0800

    Update and rename Write code to count the number of characters in original_str using the accumulation pattern and assign the answer to a variable num_chars to num_chars.py

--- num_chars.py
+++ num_chars.py
@@ -1,3 +1,5 @@
+# Write code to count the number of characters in original_str using the accumulation pattern and assign 
+# the answer to a variable num_chars
 original_str = "The quick brown rhino jumped over the extremely lazy fox."
 num_chars=0
 for i in range(len(original_str)):
